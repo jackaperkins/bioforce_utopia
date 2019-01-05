abcfa9775ab961a29d30c9c4f0ddc86cc1013307
Jack Perkins <jackaperkins@gmail.com>
2019-01-05 15:53:55 +0100

    Adjust settings, work on level1

--- Assets/ActorDriveForward.cs
+++ Assets/ActorDriveForward.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ActorDriveForward : MonoBehaviour, ITriggerable {
-
+    public float deleteDistance = 100f;
     bool triggered;
     public float speed = 10;
     public float wait = 2;
+
+    Vector3 originPos;
 	// Use this for initialization
 	void Start () {
-		
+        originPos = transform.position;
 	}
 	
 	// Update is called once per frame
@@ -21,6 +23,10 @@ public class ActorDriveForward : MonoBehaviour, ITriggerable {
         } else {
             transform.position += transform.forward * speed * Time.deltaTime;
         }
+
+        if (Vector3.Distance(originPos, transform.position) > deleteDistance){
+            gameObject.SetActive(false);
+        }
 	}
 
     public void Trigger(){
@@ -28,7 +34,9 @@ public class ActorDriveForward : MonoBehaviour, ITriggerable {
     }
 
     void OnDrawGizmosSelected () {
+
         Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(Vector3.up + transform.position + transform.forward * deleteDistance, 5);
         Gizmos.DrawLine(transform.position + Vector3.up, Vector3.up + transform.position + transform.forward * 150);
     }
 }
