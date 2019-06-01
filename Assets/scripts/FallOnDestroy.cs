91534fd5cf5ac058d1f789ce0b9c69f506cd7e42
jack perkins <jackaperkins@posteo.net>
2019-06-01 15:56:44 +0200

    update level 1, more assets, monkey preistess

---
+++ Assets/scripts/FallOnDestroy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallOnDestroy : MonoBehaviour {
+    public Transform goal;
+    public float topSpeed = 50;
+    public float acceleration = 25;
+    float speed;
+
+    Vector3 goalPosition;
+    Quaternion goalRotation;
+
+    bool triggered;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    void Destroyed() {
+        triggered = true;
+        goalPosition = goal.position;
+        goalRotation = goal.rotation;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (triggered) {
+            speed += acceleration * Time.deltaTime;
+            speed = Mathf.Min(speed, topSpeed);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, goalRotation, speed * 7 * Time.deltaTime);
+
+            transform.position = Vector3.MoveTowards(transform.position, goalPosition, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnDrawGizmos() {
+        if(goal != null) {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, goal.position);
+        }
+    }
+}
---
+++ Assets/scripts/Jiggle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jiggle : MonoBehaviour {
+
+
+    public Vector3 direction;
+    public float speed = 10;
+    public bool stopOnDestroy = true;
+
+
+
+    float rota;
+    Vector3 origin;
+	// Use this for initialization
+	void Start () {
+        origin = transform.localPosition;
+	}
+
+    public void Destroyed () {
+        if (stopOnDestroy) {
+            direction = Vector3.zero;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        rota += Time.deltaTime * speed;
+
+        transform.localPosition = origin;
+        Vector3 offset = Mathf.Sin(rota) * direction;
+        transform.Translate(offset, Space.Self);
+	}
+}
