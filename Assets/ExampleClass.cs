1f2f4dbd1741471b91acd7f0eddcc0e2d768cd74
jack perkins <jackaperkins@posteo.net>
2019-10-30 22:28:40 +0100

    add aiming guide images, small changes

---
+++ Assets/ExampleClass.cs
@@ -0,0 +1,13 @@
+﻿     using UnityEngine;
+     using System.Collections;
+     
+     public class ExampleClass : MonoBehaviour {
+         public Material mat;
+         void OnRenderImage(RenderTexture src, RenderTexture dest) {
+            for(int x =0; x< 90; x++)
+        {
+
+        }
+        Graphics.Blit(src, dest);
+         }
+     }
